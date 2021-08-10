2f9fb94a956b3f5a8a08693e23e37642ba7fef8e
prem-12345 <premkpatil1997@gmail.com>
2021-08-10 13:24:08 +0530

    UC2_THE_PLAYER_ROLLS_THE_DIE_TO_GET_A_NUMBER_BETWEEN_1_TO_6

--- src/com/prem02/snakeladder/SNAKELADDER.java
+++ src/com/prem02/snakeladder/SNAKELADDER.java
@@ -2,9 +2,26 @@ package com.prem02.snakeladder;
 
 // WELCOME TO SNAKE AND LADDER PROGRAMME
 
+/**
+ * UC2 THE PLAYER ROLLS THE DIE TO GET A NUMBER BETWEEN 1 TO 6
+ *
+ * @author prem
+ * @version 3.2
+ * @since 10/08/2021
+ *
+ */
+
 public class SNAKELADDER {
     public static void main(String[] args) {
+        int position = 0;
         System.out.println("WELCOME TO SNAKE AND LADDER PROGRAMME");
+        System.out.println("You are at Starting Position::" + position);
+        Dice();
+    }
+
+    public static void Dice(){
+        int diceCheck = (int) Math.floor(Math.random() * 10) % 6+1;
+        System.out.println("Dice Value::"+ diceCheck);
     }
 
 }
