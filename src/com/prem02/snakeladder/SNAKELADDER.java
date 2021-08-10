3304ac580f973b86cb48a48457de61646b7234c8
prem-12345 <premkpatil1997@gmail.com>
2021-08-10 12:51:06 +0530

    UC1_SNAKE_AND_LADDER_GAME_PLAYED_WITH_SINGLE_PLAYER_AT_START_POSITION_0

--- src/com/prem02/snakeladder/SNAKELADDER.java
+++ src/com/prem02/snakeladder/SNAKELADDER.java
@@ -2,9 +2,21 @@ package com.prem02.snakeladder;
 
 // WELCOME TO SNAKE AND LADDER PROGRAMME
 
+/**
+ * UC1 SNAKE AND LADDER GAME PLAYED WITH SINGLE PLAYER AT START POSITION 0
+ *
+ * @author prem
+ * @version 3.1
+ * @since 10/08/2021
+ *
+ */
+
 public class SNAKELADDER {
     public static void main(String[] args) {
+        int position = 0;
         System.out.println("WELCOME TO SNAKE AND LADDER PROGRAMME");
+        System.out.println("You are at Starting Position::" + position);
+
     }
 
 }
