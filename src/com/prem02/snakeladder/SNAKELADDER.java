9c97bfce65cb8679cc37c1b8672c97ba16a6070f
prem-12345 <premkpatil1997@gmail.com>
2021-08-10 12:39:33 +0530

    SNAKE_LADDER_PROGRAMME

---
+++ src/com/prem02/snakeladder/SNAKELADDER.java
@@ -0,0 +1,10 @@
+package com.prem02.snakeladder;
+
+// WELCOME TO SNAKE AND LADDER PROGRAMME
+
+public class SNAKELADDER {
+    public static void main(String[] args) {
+        System.out.println("WELCOME TO SNAKE AND LADDER PROGRAMME");
+    }
+
+}
