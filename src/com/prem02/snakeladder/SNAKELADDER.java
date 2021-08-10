6ffcdf4e3cca29bf9710b9f7bc96098a0dd4b2ab
prem-12345 <premkpatil1997@gmail.com>
2021-08-10 13:35:42 +0530

    UC3_THE_PLAYER_THEN_CHECK_FOR_A_OPTION_THEY_ARE_NO_PLAY_LADDER_OR_SNAKE

--- src/com/prem02/snakeladder/SNAKELADDER.java
+++ src/com/prem02/snakeladder/SNAKELADDER.java
@@ -3,25 +3,42 @@ package com.prem02.snakeladder;
 // WELCOME TO SNAKE AND LADDER PROGRAMME
 
 /**
- * UC2 THE PLAYER ROLLS THE DIE TO GET A NUMBER BETWEEN 1 TO 6
+ * UC3 THE PLAYER THEN CHECK FOR A OPTION.THEY ARE NO PLAY ,LADDER OR SNAKE
  *
  * @author prem
- * @version 3.2
+ * @version 3.3
  * @since 10/08/2021
- *
  */
 
 public class SNAKELADDER {
     public static void main(String[] args) {
-        int position = 0;
-        System.out.println("WELCOME TO SNAKE AND LADDER PROGRAMME");
-        System.out.println("You are at Starting Position::" + position);
-        Dice();
-    }
 
-    public static void Dice(){
-        int diceCheck = (int) Math.floor(Math.random() * 10) % 6+1;
-        System.out.println("Dice Value::"+ diceCheck);
+        int position = 0;
+        System.out.println("position is " + position);
+        int die = (int) (Math.random() * 10) % 6 + 1;
+        System.out.println("dice value is " + die);
+        int value = (int) (Math.random() * 10) % 3 + 1;
+        System.out.println("case number is " + value);
+
+        switch (value) {
+            case 1:
+                System.out.println("No Play You are in the same position.");
+                break;
+
+            case 2:
+                position = (position + die);
+                System.out.println("your position after ladder is ::" + position);
+                break;
+
+            case 3:
+                position = (position - die);
+
+                if (position < 0) {
+                    position = 0;
+                }
+                System.out.println("Your Position after snake is::" + position);
+                break;
+        }
     }
 
 }
