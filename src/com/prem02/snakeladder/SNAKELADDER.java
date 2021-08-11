7a864b5e44ad513d45928f967f7f17a3a0b5bc90
prem-12345 <85114766+prem-12345@users.noreply.github.com>
2021-08-11 19:42:40 +0530

    Update SNAKELADDER.java

--- src/com/prem02/snakeladder/SNAKELADDER.java
+++ src/com/prem02/snakeladder/SNAKELADDER.java
@@ -13,7 +13,10 @@ package com.prem02.snakeladder;
 
 public class SNAKELADDER {
     public static void main(String[] args) {
+        
+        //Taking position variable for starting position and assign it zero
         int position = 0;
+        
         System.out.println("WELCOME TO SNAKE AND LADDER PROGRAMME");
         System.out.println("You are at Starting Position::" + position);
 
